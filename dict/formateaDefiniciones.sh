c1909ac55a79c20b1bc6ac24a1bc304d18eaca74
Alberto Melián Rodríguez <aeronpsaro@gmail.com>
2023-05-28 16:32:34 +0100

    Fixed regex (added ñ as letter since it is not covered in [a-z])

--- dict/formateaDefiniciones.sh
+++ dict/formateaDefiniciones.sh
@@ -4,7 +4,7 @@ regex=$(cat regexDef.txt)
 
 pcre2grep -Mo $regex Todos_los_diccionarios_lematizados.txt |\
 tr "\n" "<" | tr "#" "\n" |\
-perl -p -e 's/<([a-zA-ZáéíóúÁÉÍÓÚ]+)<.*<(.*)<>([a-zA-ZáéíóúÁÉÍÓÚ]+).*<.*/\1:\3\n\2/' |\
+perl -p -e 's/<([a-zA-ZñÑáéíóúÁÉÍÓÚ]+)<.*<(.*)<>([a-zA-ZñÑáéíóúÁÉÍÓÚ]+).*<.*/\1:\3\n\2/' |\
 perl -p -e 's/^\n//' |\
 sed "s/:sustantivo/:1000/" |\
 sed "s/:adjetivo/:1100/" |\
--- dict/formateaTodos.sh
+++ dict/formateaTodos.sh
@@ -5,7 +5,7 @@ regex=$(cat regexTodos.txt)
 pcre2grep -Mo $regex Todos_los_diccionarios.txt |\
 grep -v Clave |\
 grep -vE '^[^>]+\.|###' |\
-sed "s/>\([a-zA-ZáéíóúÁÉÍÓÚ]*\).*/>\1/" |\
+sed "s/>\([a-zA-ZñÑáéíóúÁÉÍÓÚ]*\).*/>\1/" |\
 sed "s/^>/:/" |\
 perl -p -e 's/^([^:]+)\n/\1/' |\
 sed "s/:sustantivo/:1000/" |\
